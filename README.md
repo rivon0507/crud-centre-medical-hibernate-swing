ff95430c30d368c5a07949aa35973d8f67a0b097
rivon0507 <flavienrivon@gmail.com>
2025-02-19 11:00:39 +0300

    feat(hibernate_setup): Set up hibernate
    - Add appropriate dependencies
    - Add config file
    - Write tests
    - Add README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# CRUD CENTRE MEDICAL
+
+## Summary
+
+This is a simple CRUD project of three (03) tables using Hibernate and Java Swing.
+
+## How to set up
+
+- Fork the repository
+- Clone the repo
+- Run `./gradlew build` (Linux) or `gradlew.bat build` (Windows) to download the dependencies
+- copy `src/main/java/resources/hibernate.properties.example` to `src/main/java/resources/hibernate.properties` to
+  configure hibernate
+- Run the tests to ensure everything has been properly configured: `./gradlew check` for Linux or `gradlew.bat check`
+  for Windows
+- The project can now be run using `./gradlew run` (Linux) or `gradlew.bat run` (Windows)
---
+++ src/main/java/rivon0507/centremedical/util/HibernateUtil.java
@@ -0,0 +1,56 @@
+package rivon0507.centremedical.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/// A utility class that manages the hibernate session factory.
+/// The {@code boot()} method should be called before any session is opened, and the {@code shutdown()} before stopping
+/// the app.
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+    private final static Class<?>[] ENTITIES = new Class[]{
+            // Add the entity classes here
+    };
+
+    /// Creates and initializes the session factory if it is closed or null
+    public static void boot() {
+        if (isUp()) return;
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().build();
+        try {
+            sessionFactory = new MetadataSources(registry)
+                    .addAnnotatedClasses(ENTITIES)
+                    .buildMetadata()
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new RuntimeException(e);
+        }
+    }
+
+    /// Opens a session from the session factory.
+    ///
+    /// @return the opened session
+    /// @throws IllegalStateException if called before boot
+    public static Session openSession() {
+        if (!isUp())
+            throw new IllegalStateException("Utility class is not booted");
+        return sessionFactory.openSession();
+    }
+
+    /// Closes the session factory
+    public static void shutdown() {
+        if (isUp()) {
+            sessionFactory.close();
+        }
+        sessionFactory = null;
+    }
+
+    /// Queries the state of the factory
+    public static boolean isUp() {
+        return sessionFactory != null && sessionFactory.isOpen();
+    }
+}
